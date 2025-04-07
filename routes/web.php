c0dea14c4a5ca1805b01a0e381d2d5a690443ad4
yoann <yoanngervais.yg13750@gmail.com>
2025-04-07 15:09:03 +0200

    ok

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/Contact', function () {
+    return view('Contact');
+});
+
+Route::get('/Qui-sommes-nous', function () {
+    return view('Qui-sommes-nous');
+});
+
